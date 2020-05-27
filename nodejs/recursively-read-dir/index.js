ca5f6d8798a3c43c154cb3dc22cd09f4b9fd1ace
newkid519 <newkid519@hotmail.com>
2020-05-27 21:07:40 +0800

    parallel!

--- nodejs/recursively-read-dir/index.js
+++ nodejs/recursively-read-dir/index.js
@@ -1,27 +1,45 @@
+//https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
 var fs = require('fs');
 var path = require('path');
 
 var walk = function(dir, done) {
   var results = [];
   fs.readdir(dir, function(err, list) {    
-      if (err)
-          return done(err, null);
-      var i = 0;    
-      var file = list[i++];
-      while (file) {
-        file = path.resolve(dir, file);
-        results.push(file);
-        file = list[i++];
+      if (err){
+          return done(err);
       }
-      return done(null, results);      
+      var pending = list.length;
+      if (!pending) {
+        done(null, results);
+      }
+
+      list.forEach((file) => {
+        file = path.resolve(dir, file);
+        fs.stat(file, (err, stats)=> {
+          if (stats && stats.isDirectory()){
+            walk(file, (err, res)=>{
+              results = results.concat(res);
+              if (!--pending){
+                return done(null, results);
+              }
+            });
+          }else{
+            results.push(file);
+            if (!--pending){
+              return done(null, results);
+            }
+          }
+        });
+      });
   });
 };
 
-walk('C:/Users/kennyzx/Documents/GitHub/WebProgramming/nodejs/use_node_module', function(err, results) {
+walk('C:/Users/kennyzx/Documents/', function(err, results) {
     if (err) {
         //console.log(err);
         throw err;
     }
 
-    console.log(results);
+    console.log(results.length);
+    //console.log(results);
   });
